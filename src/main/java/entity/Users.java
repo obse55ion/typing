6f69ebaabcede9e0e682fe64664aa1cd22e9b438
obse55ion <daniil.kondratev1337@gmail.com>
2026-06-29 22:19:15 +0500

    structure firstcommit

---
+++ src/main/java/entity/Users.java
@@ -0,0 +1,41 @@
+package entity;
+
+public class Users {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+
+
+    public Users(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
